7e154bbaf8fee157aa0340cf37bba9d4f6ae3ac3
Di0Zavr <viktorsergeev08@gmail.com>
2023-05-25 20:25:17 +0300

    added circle and high

--- enemies.py
+++ enemies.py
@@ -1,6 +1,4 @@
 import pygame
-import random
-
 class EnemyBullet:
     def __init__(self, direction, x, y):
         self.speed = 5
@@ -33,8 +31,52 @@ class LowTurret:
         self.hitbox = pygame.Rect((self.x, self.y), (50, 24))
 
     def shot(self, array):
-        bullet = EnemyBullet(direction=self.direction, x=self.x + (1 - self.direction) * 50, y=self.y - 1)
+        bullet = EnemyBullet(direction=self.direction, x=self.x + (1 - self.direction) * 50, y=self.y + 1)
         array.append(bullet)
 
     def get_hitbox(self):
         self.hitbox = pygame.Rect((self.x, self.y), (50, 24))
+
+class HighTurret:
+    def __init__(self, x, y):
+        self.sprites = [
+            pygame.image.load('materials/images/enemies/turrets/high_turret_right.png').convert_alpha(),
+            pygame.image.load('materials/images/enemies/turrets/high_turret_left.png').convert_alpha()
+        ]
+        self.direction = 0
+        self.x = x
+        self.y = y
+        self.reload_time = 1600
+        self.timer = None
+        self.health = 6
+        self.hitbox = pygame.Rect((self.x, self.y), (50, 33))
+
+    def shot(self, array):
+        bullet = EnemyBullet(direction=self.direction, x=self.x + (1 - self.direction) * 50, y=self.y + 1)
+        array.append(bullet)
+
+    def get_hitbox(self):
+        self.hitbox = pygame.Rect((self.x, self.y), (50, 33))
+
+class CircleTurret:
+    def __init__(self, x, y):
+        self.sprites = [
+            pygame.image.load('materials/images/enemies/turrets/circle_turret.png').convert_alpha(),
+            pygame.image.load('materials/images/enemies/turrets/circle_turret.png').convert_alpha()
+        ]
+        self.direction = 0
+        self.x = x
+        self.y = y
+        self.reload_time = 1600
+        self.timer = None
+        self.health = 6
+        self.hitbox = pygame.Rect((self.x, self.y), (40, 17))
+
+    def shot(self, array):
+        bullet = EnemyBullet(direction=1, x=self.x, y=self.y + 2)
+        array.append(bullet)
+        bullet = EnemyBullet(direction=0, x=self.x + 33, y=self.y + 2)
+        array.append(bullet)
+
+    def get_hitbox(self):
+        self.hitbox = pygame.Rect((self.x, self.y), (40, 17))
