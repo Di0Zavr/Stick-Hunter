2499d1b3fd173c46776b5facf05a4a09e4ddacc4
Di0Zavr <viktorsergeev08@gmail.com>
2023-05-25 21:56:34 +0300

    nvm

--- editor.py
+++ editor.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class GameSolidObject:
     def __init__(self, x, y, path):
         self.x = x
@@ -7,10 +8,12 @@ class GameSolidObject:
         self.sprite = pygame.image.load(path)
         self.hitbox = pygame.Rect((self.x, self.y), self.sprite.get_size())
 
+
 class Level:
     def __init__(self, name):
         self.name = name
         self.game_objects_on_screen = []
+
 
 class Editor:
     def __init__(self):
