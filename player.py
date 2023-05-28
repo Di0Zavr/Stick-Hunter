93efff29bba546b032400062bf326969cb38c726
Di0Zavr <viktorsergeev08@gmail.com>
2023-05-28 20:38:16 +0300

    deleted redudant images; made getting htibox in movement methods; changed is_jump to in_air

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player:
         self.health = 10
         self.x = x
         self.y = y
-        self.is_jump = False
+        self.in_air = False
         self.shot_lock = True
         self.invincible = False
         self.jump_height = 0
@@ -19,27 +19,29 @@ class Player:
             pygame.image.load('materials/images/character/combine/player_combine_left.png').convert_alpha()
         ]
         self.bullet_icon = pygame.image.load('materials/images/character/bullet/bullet_icon.png').convert_alpha()
-        self.reload_icon = pygame.image.load('materials/images/reload_message.png').convert_alpha()
         self.hitbox = pygame.Rect((self.x, self.y), (16, 33))
         self.reload_timer = pygame.USEREVENT + 1
         self.inframes_timer = pygame.USEREVENT + 2
 
     def move_right(self, edge):
         self.x = min(self.x + self.speed, edge)
+        self.hitbox = pygame.Rect((self.x, self.y), (16, 33))
 
     def move_left(self, edge):
         self.x = max(self.x - self.speed, edge)
+        self.hitbox = pygame.Rect((self.x, self.y), (16, 33))
 
     def jump(self, gravity, ground_level):  # заметить граунд-левел на пересечение колайдеров
-        if not self.is_jump:
+        if not self.in_air:
             self.jump_height = 7
-            self.is_jump = True
+            self.in_air = True
         else:
             self.y = min(self.y - self.jump_height, ground_level)
             self.jump_height -= gravity
             if self.y == ground_level:
-                self.is_jump = False
+                self.in_air = False
                 self.jump_height = 0
+        self.hitbox = pygame.Rect((self.x, self.y), (16, 33))
 
     def single_shot(self, array):
         if not self.shot_lock and self.ammo:
@@ -64,10 +66,8 @@ class Player:
     def show_ammo(self, surf):
         w, h = self.bullet_icon.get_size()
         for i in range(self.ammo):
-            surf.blit(self.bullet_icon, (self.x + 2 + i * (w + 1), self.y - h - 2))
-    def get_hitbox(self):
-        top_left = (self.x + self.direction * 18, self.y)
-        self.hitbox = pygame.Rect(top_left, (16, 33))
+            surf.blit(self.bullet_icon, (self.x - self.direction * 15 + 2 + i * (w + 1), self.y - h - 2))
+
 
 class Bullet:
     def __init__(self, direction, x, y):
